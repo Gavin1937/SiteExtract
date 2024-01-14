c35d7936dfbc4d9bd6f22145fe6179ec565d4946
Gavin1937 <gyh1837771475gyh@gmail.com>
2024-01-13 20:03:10 -0800

    add BACKEND_URL env check

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,2 +1,4 @@
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+var BACKEND_URL = '';
+if (process.env.REACT_APP_BACKEND_URL)
+    BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 export default BACKEND_URL;
