b7df7e03400a25a1b14818d430220b5888559658
Gavin1937 <gyh1837771475gyh@gmail.com>
2024-01-14 22:39:40 -0800

    make LoginPage responsive

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -35,7 +35,7 @@ function LoginPage() {
   
   return (
     <Container
-      style={{ width: "33%", height: "60%" }}
+      style={{ width: "100%", height: "60%" }}
       className="d-flex align-items-center justify-content-center text-center min-vh-100"
     >
       <Row>
